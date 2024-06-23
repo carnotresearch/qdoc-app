25f7b6c3e9a3119d3562eef662ca31a61325aaed
ayesharifa22 <112719887+ayesharifa22@users.noreply.github.com>
2024-06-23 20:06:46 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,13 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import ReCAPTCHA from "react-google-recaptcha";
 import { useNavigate, useLocation } from "react-router-dom";
-import {
-  MDBBtn,
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBInput,
-} from "mdb-react-ui-kit";
+import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBInput } from "mdb-react-ui-kit";
 import { Spinner } from "react-bootstrap";
 import "./styles.css";
 
@@ -53,7 +47,7 @@ const Login = () => {
       console.log(response);
       const expiryTime = Date.now() + 3600 * 1000;
       sessionStorage.setItem("token", response.data.token);
-      sessionStorage.setItem("expiryTime", expiryTime.toString());
+      localStorage.setItem("expiryTime", expiryTime.toString());
       setIsLoading(false);
       navigate("/");
     } catch (error) {
@@ -67,45 +61,26 @@ const Login = () => {
     setRecaptchaToken(token);
   };
 
-  const textStyles = {
-    textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
-    // backgroundColor: "rgba(0, 0, 0, 0.3)",
-    // padding: "10px",
-    // display: "inline-block",
-    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.2)",
-    padding: "10px",
-  };
-
   return (
     <MDBContainer className="my-5 gradient-form align-items-center justify-content-center">
+      <h2 className="mb-4 ml-3 bold-text top-aligned"></h2>
       <MDBRow>
         <MDBCol lg="6" md="12" sm="12" className="mb-5">
-          <div className="d-flex flex-column  justify-content-center gradient-custom-2 h-100 mb-4 background-image">
-            <div
-              className="text-white px-3 py-4 p-md-5 mx-md-4"
-              style={textStyles}
-            >
-              <h2 className="mb-4">Welcome to Carnot Research</h2>
-              <p className="large mb-0">
-                Empowering individuals and organizations to leverage cutting
-                edge research and enable innovation. Right Mix of academics and
-                industry professionals. Key differentiator is our ability to
-                provide managed research for serving your innovation needs.
-                Active in wide variety of research areas with major focus on
-                Computer Vision and NLP.
-              </p>
-            </div>
+          <div className="background-image">
+            <p className="text-container typewriter-effect ">
+              Empowering individuals and organizations to leverage cutting edge research and enable innovation.
+              Right Mix of academics and industry professionals. Key differentiator is our ability to provide managed
+              research for serving your innovation needs. Active in wide variety of research areas with major focus on
+              Computer Vision and NLP.
+            </p>
           </div>
         </MDBCol>
         <MDBCol lg="6" md="12" sm="12" className="mb-5">
           <div className="d-flex flex-column">
             <div className="text-center">
-              <img src="/logo.png" style={{ width: "185px" }} alt="logo" />
-              <h4 className="mt-1 mb-5 pb-1">We are Carnot Research</h4>
+              <img src="/logo.png" style={{ width: "180px" }} alt="logo" />
             </div>
-
             <p>Please login to your account</p>
-
             <MDBInput
               wrapperClass="mb-4"
               label="Username"
@@ -123,15 +98,13 @@ const Login = () => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
-
             <ReCAPTCHA
               sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
               onChange={onReCAPTCHAChange}
             />
-
             <div className="text-center pt-1 mb-5 pb-1">
               <MDBBtn
-                className="mb-4 w-100 gradient-custom-2"
+                className="mb- w-100 gradient-custom-2"
                 onClick={handleSubmit}
                 disabled={isLoading}
               >
@@ -141,17 +114,7 @@ const Login = () => {
                   "Sign in"
                 )}
               </MDBBtn>
-              {/* <a className="text-muted" href="#">
-                Forgot password?
-              </a> */}
             </div>
-
-            {/* <div className="d-flex flex-row align-items-center justify-content-center pb-4 mb-4">
-              <p className="mb-0">Don't have an account?</p>
-              <MDBBtn outline className="mx-2" color="danger">
-                Register
-              </MDBBtn>
-            </div> */}
           </div>
         </MDBCol>
       </MDBRow>
