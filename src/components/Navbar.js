ccf7212a77e12686f3abba357428ccb34c4146b3
ayesharifa22 <112719887+ayesharifa22@users.noreply.github.com>
2024-06-23 20:07:07 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,22 +16,34 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" style={{ marginLeft: "0.5cm" }} to="/">
-        QDoc App
+    <nav className="navbar navbar-expand-lg" style={{  position: "relative", padding: "1rem" }}>
+      <Link
+        className="navbar-brand"
+        to="/"
+        style={{
+          position: "absolute",
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
+        Qdoc
       </Link>
-      <ul className="navbar-nav ms-auto">
+      <ul className="navbar-nav ms-auto" style={{ marginRight: "1rem" }}>
         {token ? (
           <li className="nav-item">
-            <button className="btn btn-secondary" onClick={handleLogout}>
+            <button
+              className="btn btn-logout"
+              style={{ borderColor: "black" }}
+              onClick={handleLogout}
+            >
               Logout
             </button>
           </li>
         ) : (
           <li className="nav-item">
             <button
-              style={{ marginRight: "0.5cm" }}
-              className="btn btn-primary"
+              className="btn btn-login"
+              style={{ marginRight: "0.5cm", borderColor: "black" }}
               onClick={handleLoginClick}
             >
               Login
