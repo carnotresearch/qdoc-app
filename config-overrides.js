25bf11ca55534a8ceac48bf0db63c684c5e8f10f
Pranav <pranavkanire@gmail.com>
2024-07-09 15:05:07 +0530

    pdf update

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,13 @@
 const { override, addWebpackResolve } = require("customize-cra");
 
+module.exports = function override(config, env) {
+  config.module.rules.push({
+    test: /\.mjs$/,
+    include: /node_modules/,
+    type: "javascript/auto",
+  });
+  return config;
+};
 module.exports = override(
   addWebpackResolve({
     fallback: {
