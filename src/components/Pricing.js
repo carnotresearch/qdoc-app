345549f5f7888c5f98cd4441182b1d565fb116e3
ayesharifa22 <112719887+ayesharifa22@users.noreply.github.com>
2024-09-12 21:56:48 +0530

    Update Pricing

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -55,7 +55,7 @@ const Pricing = ({ plan, darkMode }) => {
   const navigate = useNavigate();
 
   const paymentPlanOptions = [
-    { value: 1, label: "₹19/day", price: 1900 },
+   
     { value: 1, label: "₹59/week", price: 5900 },
     { value: 2, label: " ₹189/month", price: 18900 },
     { value: 3, label: " ₹479/quarter", price: 47900 },
