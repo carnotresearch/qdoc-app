a63ecad3eefd28d2e65879d5dd1e8fc0029f7e12
Pranav <pranavkanire@gmail.com>
2024-12-10 18:40:29 +0530

    styling

--- src/components/home/MiddleBlock.js
+++ src/components/home/MiddleBlock.js
@@ -1,17 +1,19 @@
 import { Slide } from "react-awesome-reveal";
 import { Button, Row, Col } from "react-bootstrap";
 import { MiddleBlockSection, Content, ContentWrapper } from "./styles";
+import { useNavigate } from "react-router-dom";
 
 const MiddleBlock = ({ setSidebarCollapsed }) => {
+  const navigate = useNavigate();
   const handleLoginClick = () => {
-    console.log("handle button click");
+    navigate("/login");
   };
   const handleTrialClick = () => {
     setSidebarCollapsed(false);
   };
 
   const content =
-    "Interact with secure knowledge container in your mother tongue";
+    "Interact with secure knowledge container in your preferred language";
   const tmStyles = {
     verticalAlign: "super",
     fontSize: "0.6rem",
@@ -20,9 +22,11 @@ const MiddleBlock = ({ setSidebarCollapsed }) => {
     fontWeight: "bolder",
   };
   const buttonStyles = {
-    marginRight: "1rem",
     backgroundColor: "#007bff",
     color: "white",
+    maxWidth: "10rem",
+    minWidth: "7rem",
+    margin: "auto",
   };
   const listStyles = {
     listStylePosition: "inside",
@@ -42,23 +46,45 @@ const MiddleBlock = ({ setSidebarCollapsed }) => {
                 </b>
               </h2>
               <Content>{content}</Content>
-              <div className="mt-3">
-                <Button
-                  name="submit"
-                  onClick={handleLoginClick}
-                  style={buttonStyles}
+              <div
+                className="container mt-3"
+                style={{
+                  padding: "auto 3rem",
+                  alignItems: "center",
+                  display: "flex",
+                  flexWrap: "wrap",
+                  justifyContent: "center",
+                  gap: "0.5rem",
+                }}
+              >
+                <div
+                  className="row"
+                  style={{
+                    padding: "0 1rem",
+                    display: "flex",
+                    gap: "1rem",
+                    flexWrap: "wrap",
+                  }}
                 >
-                  Go To Login
-                </Button>
-                <Button
-                  name="submit"
-                  onClick={handleTrialClick}
-                  style={buttonStyles}
-                >
-                  Free Trial
-                </Button>
+                  <Button
+                    className="col-lg-4 col-md-6 col-sm-12"
+                    name="submit"
+                    onClick={handleLoginClick}
+                    style={buttonStyles}
+                  >
+                    Go To Login
+                  </Button>
+                  <Button
+                    className="col-lg-4 col-md-6 col-sm-12"
+                    name="submit"
+                    onClick={handleTrialClick}
+                    style={buttonStyles}
+                  >
+                    Free Trial
+                  </Button>
+                </div>
               </div>
-              <div style={{ textAlign: "left" }} className="mt-5">
+              <div style={{ textAlign: "left", marginTop: "2rem" }}>
                 <ul style={listStyles}>
                   <li>
                     Your info remains safe and not shared externally with anyone
